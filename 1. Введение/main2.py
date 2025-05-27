367ad4453e7f18721b65b5f0f5dc678b5d756f31
Андрей Боев <a.boev.it@gmail.com>
2025-05-27 15:07:07 +0300

    Перенос старых заданий

---
+++ 1. Введение/main2.py
@@ -0,0 +1,14 @@
+"""
+print('Text 1', 'Text 2', 'Text 3', end=' ')
+print('Text 4', 'Text 5', 'Text 6')
+"""
+from math import sqrt
+
+"""
+print('\'Text 1\'', 'Text 2', 'Text 3', 'Text 4', 'Text 5', 'Text 6', sep='\n')
+"""
+
+num_1 = int(input('Введите число 1: '))
+num_2 = int(input('Введите число 2: '))
+
+print(sqrt(num_1 + num_2))
